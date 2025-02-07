501bfaa18cecd6574d5a2f8421a98283114321f0
HelenaLC <helena@crowell.eu>
2025-02-07 09:28:04 +0100

    +split by sample

--- code/10-plt__all_sid__all_sid_all_sub-roi,lv2,fq_by_typ.R
+++ code/10-plt__all_sid__all_sid_all_sub-roi,lv2,fq_by_typ.R
@@ -15,41 +15,54 @@ df <- mapply(x=rep(args[[1]], each=3), y=args[[2]], \(x, y) {
     sub <- gsub(".*(epi|imm|str).*", "\\1", y)
     kid <- (k <- ist$clust)[match(colnames(sce), names(k))]
     data.frame(colData(sce)[c("sid", "typ")], sub, kid)
-}, SIMPLIFY=FALSE) |> do.call(what=rbind) 
-
-# wrangling
-fd <- df |>
+}, SIMPLIFY=FALSE) |> 
+    do.call(what=rbind) |>
+    mutate(roi=typ) |>
+    mutate(sid=gsub("^([0-9]{3}).*", "\\1", roi)) |>
     mutate(typ=gsub(".*(REF|TVA|CRC).*", "\\1", typ)) |>
     mutate(typ=factor(typ, c("REF", "TVA", "CRC"))) |>
-    filter(!is.na(typ), !is.na(kid)) |> 
-    # count cells by cluster & region
-    group_by(sub, kid, typ) |> tally() |>
-    # get relative proportion by cluster
-    mutate(p=n/sum(n)) |> ungroup()
+    filter(!is.na(typ), !is.na(kid))
 
-# order by REF fraction
-xo <- fd |>
-    filter(typ == "REF") |> 
-    arrange(p) |> pull(kid) |> unique()
-fd <- mutate(fd, kid=factor(kid, xo))
-
-# plotting
-gg <- ggplot(fd, aes(p, kid, fill=typ)) + 
-    facet_grid(rows="sub", scales="free_y", space="free") +
-    geom_col(alpha=2/3, position="fill", width=1, key_glyph="point") +
-    scale_fill_manual(values=c("limegreen", "royalblue", "tomato")) +
+# aesthetics
+aes <- list(
+    scale_fill_manual(values=.pal_roi),
+    facet_grid(rows="sub", scales="free_y", space="free"),
+    geom_col(alpha=2/3, position="fill", width=1, key_glyph="point"),
     scale_x_continuous(
         "proportion of cells", n.breaks=6, 
-        labels=scales::label_percent()) +
-    coord_cartesian(expand=FALSE) +
-    theme_bw(6) + theme(
+        labels=scales::label_percent()),
+    coord_cartesian(expand=FALSE),
+    .thm_fig_d("minimal"), theme(
         legend.position="none",
         panel.grid=element_blank(),
-        axis.ticks=element_blank(),
-        axis.title.y=element_blank(),
-        plot.background=element_blank(),
-        panel.background=element_blank(),
-        strip.background=element_rect(fill=NA))
+        axis.title.y=element_blank()))
+
+# reorder according to REF proportion
+foo <- \(df) {
+    o <- df |>
+        filter(typ == "REF") |> 
+        arrange(p) |> pull(kid) |> unique()
+    mutate(df, kid=factor(kid, o))
+}
+
+# joint
+fd <- df |>
+    group_by(sub, kid, typ) |> tally() |>
+    mutate(p=n/sum(n)) |> ungroup() 
+p0 <- ggplot(foo(fd), aes(p, kid, fill=typ)) + 
+    aes + ggtitle(.lab("", sum(fd$n)))
+
+# split
+fd <- df |>
+    group_by(sub, kid, sid, typ) |> tally() |>
+    mutate(p=n/sum(n)) |> ungroup() 
+ps <- by(fd, fd$sid, \(.) {
+    ggplot(foo(.), aes(p, kid, fill=typ)) + 
+    aes + ggtitle(.lab(.$sid[1], sum(.$n)))
+})
 
 # saving
-ggsave(args[[3]], gg, units="cm", width=6, height=8)
+gs <- gridExtra::marrangeGrob(
+    grobs=c(list(p0), ps), 
+    nrow=1, ncol=1, top=FALSE)
+ggsave(args[[3]], gs, unit="cm", width=6, height=8)
